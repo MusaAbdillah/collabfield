3867a526fe2a34929641a97ee9b8150ed0649212
MusaAbdillah <musaabdillahm9@gmail.com>
2018-03-29 00:23:23 +0700

    add specs for the post models validation

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -52,4 +52,53 @@ RSpec.describe Post, type: :model do
 
 	end
 
+	context "Validatin" do
+		let(:post) { build(:post)}
+		
+		it "creates successfully" do
+			expect(post).to be_valid 
+		end 
+
+		it "is not valid without a category" do 
+			post.category_id = nil 
+			expect(post).not_to be_valid
+		end
+
+		it "is not valid without a title" do
+			post.title = nil 
+			expect(post).not_to be_valid 
+		end
+
+		it "is not valid without a user_id" do
+			post.user_id = nil 
+			expect(post).not_to be_valid 
+		end
+
+		it "it not valid with title, shorter than 5 characters" do
+			post.title = "a" * 4
+			expect(post).not_to be_valid
+		end
+
+		it "it not valid with title, longer than 255 characters" do
+			post.title = "a" * 256
+			expect(post).not_to be_valid
+		end
+
+		it "is not valid without a content" do
+			post.content = nil 
+			expect(post).not_to be_valid 
+		end
+
+		it "is not valid with content, shorter than 20 characters" do
+			post.content = "a" * 10
+			expect(post).not_to be_valid 
+		end
+
+		it "is not valid with content, longer than 1000 characters" do
+			post.content = "a" * 1001
+			expect(post).not_to be_valid 
+		end
+
+	end
+
 end
